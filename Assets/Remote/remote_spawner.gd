ccf13456cffec41650c02ff2a33c49e71727e3fb
Rick van Loo <mail@rickvanloo.nl>
2025-10-20 17:55:27 +0200

    Fix remote spawner + added locations

--- Assets/Remote/remote_spawner.gd
+++ Assets/Remote/remote_spawner.gd
@@ -5,16 +5,24 @@ extends Node3D
 @export var click_score: int = 1
 @export var debug_value: int = 0
 
-@onready var Remote = get_node("Remote")
+@onready var Remote = get_node("Remote/RigidBody3D")
 @onready var Locations = get_node("Locations").get_children(true)
+@onready var spark_sound: AudioStreamPlayer3D = $Remote/RigidBody3D/AudioStreamPlayer3D
 
 var current_location_index = 0
 
 ## setPosition sets the "Remote" position to one of the set Marker3Ds within "Locations".
 func setPosition(index: int) -> void:
+	print("Setting to "+str(index)+" name: "+str(Locations[index].name))
+	
+	
 	current_location_index = index
+	# Wait for the scene tree to process
+	await get_tree().process_frame
+
 	Remote.global_position = Locations[index].global_position
 	Remote.global_rotation = Locations[index].global_rotation
+	spark_sound.play()
 
 ## getSemiRandomIndex returns a "random" index of the locations, but makes sure it's different than the previous.
 func getSemiRandomLocation() -> int:
